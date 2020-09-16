b1cce7e9d392a42cf1c84289d22f726e0287df81
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2020-09-16 15:33:31 +0100

    Clean up script and clarify where user changes are expected

--- kilosort2_script.m
+++ kilosort2_script.m
@@ -1,30 +1,44 @@
+%% Script to control the running of Kilosort2 on cortical and thalamic data
 
-
-
+% This is the only info that should need manual entry for each data set:
 data_dir        = 'C:\Spike_sorting\preprocessed_data\NB151 200127';
 
-%%
+% The directory of the Spike2_Kilosort code repository
+repo_dir        = 'C:\Spike_sorting\GitHub\Spike2_Kilosort';
+
+
+%% These should not need to be edited
+
+% Where should temporary files be stored (best on a fast SSD)
+temp_loc                = 'C:\Spike_sorting\temp';
+
+% Where are the Kilosort-compatible binary files?
 cortex_data_dir         = [data_dir filesep 'cortex'];
 thalamus_data_dir       = [data_dir filesep 'thalamus'];
 
-cortex_config_file      = 'C:\Spike_sorting\GitHub\Spike2_Kilosort\cortex_config\cortex_config_KS2.m';
-thalamus_config_file   	= 'C:\Spike_sorting\GitHub\Spike2_Kilosort\thalamus_config\thalamus_config_KS2.m';
+% These refer to the sub-folders in the repository with the cortex- and
+% thalamus-specific channelmaps and settings for Kilosort2
+cortex_config_dir       = [repo_dir filesep 'cortex_config'];
+thalamus_config_dir     = [repo_dir filesep 'thalamus_config'];
 
-cortex_chan_map_file    = 'C:\Spike_sorting\GitHub\Spike2_Kilosort\cortex_config\CortexMap.mat';
-thalamus_chan_map_file	= 'C:\Spike_sorting\GitHub\Spike2_Kilosort\thalamus_config\ThalamusMap.mat';
+% Kilosort2 config files
+cortex_config_file      = [cortex_config_dir filesep 'cortex_config_KS2.m'];
+thalamus_config_file   	= [thalamus_config_dir filesep 'thalamus_config_KS2.m'];
 
-temp_loc                = 'C:\Spike_sorting\temp';
+cortex_chan_map_file    = [cortex_config_dir filesep 'CortexMap.mat'];
+thalamus_chan_map_file	= [thalamus_config_dir filesep 'ThalamusMap.mat'];
+
 
 %% Run kilosort on cortical and thalamic data
 start_time      = clock;
 disp('Starting Kilosort2 spike sorting of cortical data...')
-run_kilosort(cortex_data_dir, cortex_config_file, cortex_chan_map_file, temp_loc)
+run_kilosort2(cortex_data_dir, cortex_config_file, cortex_chan_map_file, temp_loc)
 cortex_time 	= etime(clock, start_time);
 disp(['Finished Kilosort2 spike sorting of cortical data in ' num2str(cortex_time) ' seconds'])
 
 thalamus_start_time = clock;
 disp('Starting Kilosort2 spike sorting of thalamic data...')
-run_kilosort(thalamus_data_dir, thalamus_config_file, thalamus_chan_map_file, temp_loc)
+run_kilosort2(thalamus_data_dir, thalamus_config_file, thalamus_chan_map_file, temp_loc)
 thalamus_time   = etime(clock, thalamus_start_time);
 disp(['Finished Kilosort2 spike sorting of thalamic data in ' num2str(cortex_time) ' seconds'])
 
