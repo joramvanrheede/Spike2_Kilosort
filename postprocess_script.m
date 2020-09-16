726e17178eee00656662b17900f214382e5cd035
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2020-09-16 18:02:03 +0100

    Clarify / reorganise script to make it clearer which variables should require user input

--- postprocess_script.m
+++ postprocess_script.m
@@ -1,8 +1,13 @@
 %% Example script for running the postprocess_kilosort function
 
-% Filename to save the synched & sorted data
-sorted_data_save_name       = 'Experiment_ID_Sorted.mat';
+session_ID        	= 'NB151 200127'; % This should be the experiment folder name
 
-kilosort_folder             = '/Users/Joram/Data/Sharott/Kilosort_binary';
+data_folder         = 'C:\Spike_sorting\preprocessed_data';
+
+%% 
+kilosort_folder     = [data_folder filesep session_ID];
+
+% Filename to save the synched & sorted data
+sorted_data_save_name       = [session_ID '_sorted.mat'];
 
 postprocess_kilosort(kilosort_folder, sorted_data_save_name)
